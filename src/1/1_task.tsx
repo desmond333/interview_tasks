8b09178b85575ae4a2e2f08d36408af8d5759d00
Slava Vorobev <my_post_office1@mail.ru>
2023-04-25 12:53:57 +0300

    feat/#1: add 1 task

---
+++ src/1/1_task.tsx
@@ -0,0 +1,69 @@
+// Условия задачи:
+// сделать так, чтобы в button можно было передавать colors связанные только с одним вариантом и ts ругался
+
+// Variant 'one' colors:
+// - green
+// - red
+// - blue
+
+// Variant 'two' colors:
+// - green
+// - yellow
+// - black
+
+const Button = () => {
+    return <button>Button</button>
+}
+
+export const Task1 = () => {
+    return (
+        <main>
+            <Button variant='one' color='yellow' />
+        </main>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+// Условия задачи:
+// сделать так, чтобы в button можно было передавать colors связанные только с одним вариантом и ts ругался
+
+// Variant 'one' colors:
+// - green
+// - red
+// - blue
+
+// Variant 'two' colors:
+// - green
+// - yellow
+// - black
+
+// Решение
+// type ColorGreen = 'green'
+// type ColorBlue = 'blue'
+// type ColorRed = 'red'
+// type ColorYellow = 'yellow'
+// type ColorBlack = 'black'
+//
+// type VariantOne = {
+//     variant: 'one'
+//     color: ColorGreen | ColorRed | ColorBlue
+// }
+//
+// type VariantTwo = {
+//     variant: 'two'
+//     color: ColorGreen | ColorYellow | ColorBlack
+// }
+//
+// type ButtonProps = VariantOne | VariantTwo
+
+// const Button = ({ variant, color }: ButtonProps) => {
+//     return <button>Button</button>
+// }
